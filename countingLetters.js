f630ee8e81c31967b8cccb7524cec90c2b2335d9
Kevin Holmes <kevinholmes921@gmail.com>
2022-05-04 23:47:39 +0000

    implementing counting letters

---
+++ countingLetters.js
@@ -0,0 +1,18 @@
+const countingLetters = function(string){
+  let output = {}
+  for (const letter of string) {
+    if (letter !== " ") {
+     if (!output[letter]) {
+      output[letter] = 1;
+    } else { 
+      output[letter] += 1;
+    }
+  }
+  }
+  return output;
+};
+
+console.log(countingLetters("My name is Kevin"))
+//LHL
+//L :2
+//H: 1
