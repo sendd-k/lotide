bfc34dee1d33aabf079b6209d4d3f6b28561ac60
Kevin Holmes <kevinholmes921@gmail.com>
2022-05-04 23:08:20 +0000

    counting

---
+++ countOnly.js
@@ -0,0 +1,44 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertation Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(` 🛑🛑🛑 Assertion Failed:  ${actual} !==  ${expected}`);
+  }
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  let subset = {};
+  for(item in itemsToCount)
+    if(itemsToCount[item]) {
+      let count = 0;
+      for (let i = 0; i <allItems.length; i++) {
+        if(allItems[i] === item){
+          count += 1
+        }
+      }
+      if(count !== 0) {
+        subset[item] = count
+      }
+    }
+    return subset;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
+
