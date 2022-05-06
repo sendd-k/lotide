dfce889b4e47767540565d5b7b4722d596c67640
Kevin Holmes <kevinholmes921@gmail.com>
2022-05-06 00:51:31 +0000

    implementing findKey

---
+++ findKey.js
@@ -0,0 +1,40 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertation Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(` 🛑🛑🛑 Assertion Failed:  ${actual} !==  ${expected}`);
+  }
+};
+
+//code
+
+function findKey(object, callback) {
+  let star = Object.keys(object);
+  for (let key of star) {
+    if (callback(object[key])) {
+      console.log(key);
+      return key;
+    }
+  }
+  return undefined;
+}
+
+findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    "Akaleri": { stars: 3 },
+    "noma": { stars: 2 },
+    "elBulli": { stars: 3 },
+    "Ora": { stars: 2 },
+    "Akelarre": { stars: 3 },
+  },
+  (x) => x.stars === 2
+); // => "noma"
+
+assertEqual(findKey(
+  {
+  "Tiger": {strength: 6}, 
+  "Bear": {strength: 8}, 
+  "Fox": {strength: 3},
+  "Wolf": {strength: 5}, 
+}, (x) => x.strength === 8), "Bear");
