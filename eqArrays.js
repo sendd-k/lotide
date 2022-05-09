4e9d6182aac82e0150123af7207fd14578962e27
Kevin Holmes <kevinholmes921@gmail.com>
2022-05-09 22:06:13 +0000

    refactoring eqArrays for testing

--- eqArrays.js
+++ eqArrays.js
@@ -1,25 +1,14 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertation Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(` 🛑🛑🛑 Assertion Failed:  ${actual} !==  ${expected}`);
+const eqArrays = function(array1, array2) {
+  let output = true;
+  if (array1.length !== array2.length) return false;
+  for (let element = 0; element < array1.length; element += 1) {
+    if (Array.isArray(array1[element]) || Array.isArray(array2[element])) {
+      output = output && eqArrays(array1[element], array2[element]);
+    } else if (array1[element] !== array2[element]) {
+      output = output && false;
+    }
   }
+  return output;
 };
 
-assertEqual('Lighthouse Labs', 'Bootcamp');
-assertEqual(1, 1);
-
-const eqArrays =function(array1, array2){
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  return true; 
-}
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+module.exports = eqArrays;
