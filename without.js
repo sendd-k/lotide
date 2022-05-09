5bf28f5d7337ff245f34e08d6920aa04cb765800
Kevin Holmes <102200682+sendd-k@users.noreply.github.com>
2022-05-09 16:13:57 -0700

    Update without.js

--- without.js
+++ without.js
@@ -1,8 +1,15 @@
-const eqArrays =function(array1, array2){
-  if (array1.length !== array2.length) {
-    return false;
+const eqArrays = function(actual, expected) {
+  let output = true;
+  if (actual.length !== expected.length) return false;
+  
+  for (let element = 0; element < actual.length; element += 1) {
+    if (Array.isArray(actual[element]) || Array.isArray(expected[element])) {
+      output = output && eqArrays(actual[element], expected[element]);
+    } else if (actual[element] !== expected[element]) {
+      output = output && false;
+    }
   }
-  return true; 
+  return output;
 };
 
 const assertArraysEqual = function(array1, array2) {
