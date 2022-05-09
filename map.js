0c05920ba853bfce11f9424bb8ba039046eb1705
Kevin Holmes <102200682+sendd-k@users.noreply.github.com>
2022-05-09 16:13:37 -0700

    Update map.js

--- map.js
+++ map.js
@@ -1,7 +1,16 @@
-const eqArrays = (arrayOne, arrayTwo) => {
-  const isEqual = JSON.stringify(arrayOne) === JSON.stringify(arrayTwo);
-  return isEqual;
-}
+const eqArrays = function(actual, expected) {
+  let output = true;
+  if (actual.length !== expected.length) return false;
+  
+  for (let element = 0; element < actual.length; element += 1) {
+    if (Array.isArray(actual[element]) || Array.isArray(expected[element])) {
+      output = output && eqArrays(actual[element], expected[element]);
+    } else if (actual[element] !== expected[element]) {
+      output = output && false;
+    }
+  }
+  return output;
+};
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
