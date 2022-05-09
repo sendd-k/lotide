f3f22eee9efa51fd71401eae2d489b94f7097ef8
Kevin Holmes <kevinholmes921@gmail.com>
2022-05-09 22:25:59 +0000

    refactoring middle for testing

--- middle.js
+++ middle.js
@@ -1,18 +1,3 @@
-const eqArrays =function(array1, array2){
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  return true; 
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅ Assertation Passed: ${actual} === ${expected}`);
-  } else if (eqArrays(array1, array2)) {
-    console.log(` 🛑🛑🛑 Assertion Failed:  ${actual} !==  ${expected}`);
-  }
-};
-
 const middle = function(array) {
   if (array.length <= 2) {
     return [];
@@ -25,3 +10,5 @@ const middle = function(array) {
     return [array[index3]];
   }
 };
+
+module.exports = middle;
