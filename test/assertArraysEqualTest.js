d3d8055643a6a898ff739f9ebec0b4ef146c6b9d
Kevin Holmes <102200682+sendd-k@users.noreply.github.com>
2022-05-09 17:17:19 -0700

    eslinted

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,8 +1,8 @@
 const assertArraysEqual = require('../assertArraysEqual');
 
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); 
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
 assertArraysEqual(["My", "name", "is", "Kevin"], ["My", "name", "is", "Kevin"]);
 
-assertArraysEqual([2, 2, 6], [2, 4, 6]); 
-assertArraysEqual([2, 4, 6], [2, 4, "6"]); 
+assertArraysEqual([2, 2, 6], [2, 4, 6]);
+assertArraysEqual([2, 4, 6], [2, 4, "6"]);
