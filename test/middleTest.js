581beb69ca3f8ea40c19f1a400ce345b07a36bec
Kevin Holmes <102200682+sendd-k@users.noreply.github.com>
2022-05-09 17:18:19 -0700

    eslinted

--- test/middleTest.js
+++ test/middleTest.js
@@ -6,12 +6,12 @@ describe("#middle", () => {
     assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
   });
   it("returns [3] fpr [1, 2, 3, 4, 5]", () => {
-    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]); 
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
   });
   it("returns [2, 3] for [1, 2, 3, 4]", () => {
     assert.deepEqual(middle([1, 2, 3, 4, 5]), [3, 4]);
   });
   it("returns [3] fpr [1, 2, 3, 4, 5]", () => {
-    assert.deepEqual(middle([1, 2, 3, 4, 5]), [4]); 
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [4]);
   });
 });
