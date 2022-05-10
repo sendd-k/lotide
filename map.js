fb520e424f6ab6c98b5d9755f76fd394d21d53de
Kevin Holmes <102200682+sendd-k@users.noreply.github.com>
2022-05-09 17:15:35 -0700

    eslinted

--- map.js
+++ map.js
@@ -15,7 +15,7 @@ const eqArrays = function(actual, expected) {
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
     console.log(`✅✅✅ Assertation Passed: ${array1} === ${array2}`);
-  } else if (eqArrays(array1, array2)) {
+  } else {
     console.log(` 🛑🛑🛑 Assertion Failed:  ${array1} !==  ${array2}`);
   }
 };
@@ -24,12 +24,12 @@ const words = ["ground", "control", "to", "major", "tom"];
 
 const map = function(array, callback) {
   const results = [];
-  for(let item of array) {
-  results.push(callback(item));
+  for (let item of array) {
+    results.push(callback(item));
   }
   return results;
 
-}
+};
 
 const results1 = map(words, word => word[0]);
 console.log(results1);
