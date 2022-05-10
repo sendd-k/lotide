3513f7dbc2343417d5a22c5a21d0bd0a716e11d1
Kevin Holmes <102200682+sendd-k@users.noreply.github.com>
2022-05-09 17:09:58 -0700

    eslinted

--- assertObjectEqual.js
+++ assertObjectEqual.js
@@ -1,20 +1,20 @@
 const eqObjects = function(object1, object2) {
-  let key1 = Object.keys(object1)
-  let key2 = Object.keys(object2)
-  if(key1.length !== key2.length ) { //checking if number of keys is same 
+  let key1 = Object.keys(object1);
+  let key2 = Object.keys(object2);
+  if (key1.length !== key2.length) {
     return false;
-  } else { 
+  } else {
     for (const key of key1) {
       if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-          if (eqArrays(object1[key], object2[key])) {
-            return true;
-          }
-          return false;
+        if (eqObjects(object1[key], object2[key])) {
+          return true;
+        }
+        return false;
       }
       
       
       
-          if (object1[key] !== object2[key]) { // checking if the values are the same 
+      if (object1[key] !== object2[key]) {
         return false;
       }
     }
@@ -25,9 +25,9 @@ const eqObjects = function(object1, object2) {
 
 // FUNCTION IMPLEMENTATION
 const assertObjectsEqual = function(array1, array2) {
-  const inspect = require("util").inspect
+  const inspect = require("util").inspect;
   if (eqObjects(array1, array2)) {
-  console.log(`✅✅✅ Assertation Passed: ${inspect(array1)} = ${inspect(array2)}`);
+    console.log(`✅✅✅ Assertation Passed: ${inspect(array1)} = ${inspect(array2)}`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${inspect(array1)} != ${inspect(array2)}`);
   }
